69cc1cbe7c4619790cc9f86c983c22fe751e02b2
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-24 21:57:00 +0400

    add hover animation

--- modern_bank_landing_ui/src/components/Clients.jsx
+++ modern_bank_landing_ui/src/components/Clients.jsx
@@ -6,7 +6,7 @@ const Clients = () => (
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map((client)=>(
         <div  key={client.id} className={`${styles.flexCenter} flex-1 sm:min-w[192px] min-w-[120px]`}>
-          <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain " />
+          <img src={client.logo} alt="client" className=" sm:w-[192px] w-[100px] object-contain scale-[1] duration-300 hover:scale-[1.134]" />
         </div>
       ))}
     </div>
